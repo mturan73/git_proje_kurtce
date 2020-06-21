48fac91383f3b948b04cee68b5c83d9c5eff87b2
mturan73 <67124565+mturan73@users.noreply.github.com>
2020-06-21 15:07:57 +0300

    Update FirstClass.java
    conflict yaptık

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("birinci commit from local repository");
 		System.out.println("ikinci commit from local repository");
-		System.out.println("ucuncu commit from local repository");
+		System.out.println("conflict yapmak istiyorum");
 		System.out.println("birinci commit from remote repository");
 
 	}
